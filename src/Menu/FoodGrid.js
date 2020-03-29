e13b74302fd33c528b190e7ee281ad49ad0ab29f
alongkot <nathankc94@gmail.com>
2020-03-28 23:36:29 -0500

    add css on food hover

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -21,10 +21,17 @@ font-size: 20px;
 background-image: ${({img}) => `url(${img});`}
 background-position: center;
 background-size: cover;
-filter:  contrast(75%);
+filter: contrast(75%); 
 border-radius: 7px;
+margin-top: 5px; 
+transition-property: box-shadow margin-top filter; 
+transition-duration: .1s; 
+box-shadow: 0px 0px 2px 0px grey;
 &:hover {
-    cursor: pointer;
-    opacity: 0.7;
+ cursor: pointer; 
+ filter: contrast(100%); 
+ margin-top: 0px; 
+ margin-bottom: 5px; 
+ box-shadow: 0px 5px 10px 0px grey;
 }
 `
