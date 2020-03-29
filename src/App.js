a43c3a6fc002d3491094de6110a552d753e9a82c
alongkot <nathankc94@gmail.com>
2020-03-28 20:44:52 -0500

    navbar, color

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createGlobalStyle } from "styled-components"
-
+import { Navbar } from "./Navbar/navbar";
 const GlobalStyle = createGlobalStyle`
   body {
+    margin: 0;
     font-family: 'Abel', sans-serif;
   }
 
@@ -15,6 +16,7 @@ function App() {
   return (
     <>
     <GlobalStyle/>
+    <Navbar/>
     <h1>Noi's Kitchen</h1>
    <div>Food are here</div>
 
